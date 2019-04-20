087976b9125360bd5081bef3c8f928c842abf35a
graupv <38445832+graupv@users.noreply.github.com>
2019-04-20 16:04:52 -0600

    tableros

---
+++ src/board.s
@@ -0,0 +1,23 @@
+/*printings*/
+
+.global
+.align 2
+.text
+
+line:
+    /*
+    symb    code
+    |   ->  124
+    -   ->  045
+
+    cuadro consiste en
+
+     --- --- --- --- 
+    | R | R | R | R |
+     --- --- --- --- 
+    */
+
+table:
+    /*
+    read 
+    */
